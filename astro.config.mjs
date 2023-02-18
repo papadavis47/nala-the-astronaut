f26e05f2e8d9301a06a5c90fe481011767787b6f
John William Davis <jwdavisdeveloper@gmail.com>
2023-02-18 02:59:02 -0800

    add mdx, 2 more pages, work on global css and specific css

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,11 @@ import image from '@astrojs/image';
 // https://astro.build/config
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [image({ serviceEntryPoint: '@astrojs/image/sharp' })],
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), mdx()]
 });
