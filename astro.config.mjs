ebf5ea7c77ac21c2f1958ac04c221710aad592bf
John William Davis <jwdavisdeveloper@gmail.com>
2025-06-21 21:02:35 -0700

    use claude to switch to pnpm, update Astro to latest as well as all other dependencies and convert project to tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,20 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import image from '@astrojs/image';
-
-// https://astro.build/config
-
-// https://astro.build/config
 import mdx from '@astrojs/mdx';
-
-// https://astro.build/config
 import react from '@astrojs/react';
-
-// https://astro.build/config
 import solidJs from '@astrojs/solid-js';
+import icon from 'astro-icon';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
+    tailwind(),
     mdx(),
-    react(),
-    solidJs(),
+    react({
+      include: ['**/react/**/*'],
+    }),
+    solidJs({
+      include: ['**/solid/**/*'],
+    }),
+    icon(),
   ],
 });
