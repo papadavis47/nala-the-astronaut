96e6b907315071dc45e0865251f60f9ad840755d
John William Davis <jwdavisdeveloper@gmail.com>
2025-06-21 21:16:35 -0700

    improve typescript with the help of claude code

---
+++ src/types/images.d.ts
@@ -0,0 +1,34 @@
+declare module "*.jpg" {
+  const value: ImageMetadata;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: ImageMetadata;
+  export default value;
+}
+
+declare module "*.png" {
+  const value: ImageMetadata;
+  export default value;
+}
+
+declare module "*.webp" {
+  const value: ImageMetadata;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: ImageMetadata;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: ImageMetadata;
+  export default value;
+}
+
+declare module "*.avif" {
+  const value: ImageMetadata;
+  export default value;
+}
